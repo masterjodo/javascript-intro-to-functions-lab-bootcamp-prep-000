2ead5710544232be81ce62170c84631468522522
Jawad Ashour <jawadashour@gmail.com>
2018-12-21 17:38:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === 'hello') {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  else if (string.toUpperCase() === 'HELLO') {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {

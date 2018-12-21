7f43fd3c081e571f88e96b04339127fad5d7a3d5
Jawad Ashour <jawadashour@gmail.com>
2018-12-21 17:30:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,4 @@ function sayHiToGrandma(string) {
   if string.toLowerCase() === 'hello' {
     return "I can't hear you!"
   }
-  else string.toUpperCase() === 'HELLO' {
-    return "YES INDEED!"
-  }
-  else string === "I love you, Grandma." {
-    return "I love you, too."
-  }
-}
+
